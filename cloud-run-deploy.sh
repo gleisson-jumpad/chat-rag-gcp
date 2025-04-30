14b449eaf41225f8f5a01448c571e2e85122ed1f
Gleisson Cabral - Jumpad <gleisson.cabral@jumpad.ai>
2025-04-29 23:47:48 -0300

    Update cloud-run-deploy.sh to switch Docker image storage from Google Container Registry to Artifact Registry. Modify image tags for consistency and enhance deployment messages for better user feedback.

--- cloud-run-deploy.sh
+++ cloud-run-deploy.sh
@@ -11,12 +11,12 @@ echo "Using connection name: $INSTANCE_CONNECTION_NAME"
 
 # Build the container
 echo "🔧 Building container image..."
-gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
+gcloud builds submit --tag us-east4-docker.pkg.dev/$PROJECT_ID/chat-rag/image:latest
 
 # Deploy to Cloud Run
 echo "🚀 Deploying to Cloud Run..."
 gcloud run deploy $SERVICE_NAME \
-  --image gcr.io/$PROJECT_ID/$SERVICE_NAME \
+  --image us-east4-docker.pkg.dev/$PROJECT_ID/chat-rag/image:latest \
   --platform managed \
   --region $REGION \
   --set-env-vars="PG_DB=postgres,PG_USER=postgres,PG_PASSWORD=AMGQk@50l9Eu,DB_PUBLIC_IP=34.48.95.143" \
@@ -26,4 +26,3 @@ gcloud run deploy $SERVICE_NAME \
   --service-account="682048092511-compute@developer.gserviceaccount.com"
 
 echo "✅ Deployment complete!"
-echo "testando"
